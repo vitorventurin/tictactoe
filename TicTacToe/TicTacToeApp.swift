74067ff29d182b7245e697dd74c05314dc52ce0a
rotiv <vitorvl@yahoo.com.br>
2021-03-11 14:39:16 +0100

    1st cmmt - swift ui

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Rotiv on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
