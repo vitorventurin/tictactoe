92611ba9e2dd843fd785057c7b4c51c30e7ba3fa
rotiv <vitorvl@yahoo.com.br>
2021-03-11 15:47:45 +0100

    tdd

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -9,25 +9,81 @@ import XCTest
 @testable import TicTacToe
 
 class TicTacToeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testNewPosition() throws {
+        let position = Position()
+        XCTAssertEqual(position.turn, "x")
+        XCTAssertEqual("         ", position.description)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMove() throws {
+        let position = Position().move(1)
+        XCTAssertEqual(position.turn, "o")
+        XCTAssertEqual(" x       ", position.description)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testUnmove() {
+        let position = Position().move(1).unmove(1)
+        XCTAssertEqual(position.turn, "x")
+        XCTAssertEqual("         ", position.description)
     }
+    
+    func testPossibleMoves() {
+        let position = Position()
+        var list = [Int]()
+        list.reserveCapacity(position.size)
+        for i in 0..<position.size {
+            list.append(i)
+        }
+        if let indexOf1 = list.firstIndex(of: 1) {
+            list.remove(at: indexOf1)
+        }
+        if let indexOf2 = list.firstIndex(of: 2) {
+            list.remove(at: indexOf2)
+        }
+        XCTAssertEqual(list, Position().move(1).move(2).possibleMoves())
+    }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class Position: CustomStringConvertible {
+    var turn: Character
+    var board: [Character]
+    var dimension = 3
+    var size: Int
+    
+    init() {
+        turn = "x"
+        size = dimension * dimension
+        board = []
+        board.reserveCapacity(size)
+        for _ in 0..<size {
+            board.append(" ")
         }
     }
-
+    
+    func move(_ index: Int) -> Position {
+        board[index] = turn
+        turn = turn == "x" ? "o" : "x"
+        return self
+    }
+    
+    func unmove(_ index: Int) -> Position {
+        board[index] = " "
+        turn = turn == "x" ? "o" : "x"
+        return self
+    }
+    
+    func possibleMoves() -> [Int] {
+        print(board.description)
+        var list = [Int]()
+        for i in 0..<board.count {
+            if board[i] == " " {
+                list.append(i)
+            }
+        }
+        return list
+    }
+    
+    var description: String {
+        return board.map { "\($0)" }.joined()
+    }
 }
